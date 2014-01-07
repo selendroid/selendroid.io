38d230d5d8cca2517fd1ebe997429f78f5c0a71a
Dominik Dary <ddary@ebay.com>
2014-01-07 21:28:47 +0100

    adding redirect handling.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,9 +1,14 @@
 require 'bootstrap-sass'
+require 'redirect_creator'
 
 Awestruct::Extensions::Pipeline.new do
   helper Awestruct::Extensions::GoogleAnalytics
+  Awestruct::Extensions::Pipeline.new do
+    extension Awestruct::Extensions::Sitemap.new
+  end
   # extension Awestruct::Extensions::Posts.new '/news'
   # extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
   # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+  extension Awestruct::Extensions::RedirectCreator.new
 end
