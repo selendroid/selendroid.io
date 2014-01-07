846ffb64a5b7945539fb9bdd061fdb68d792f8e1
Dominik Dary <ddary@ebay.com>
2014-01-07 21:56:48 +0100

    adding sitemap

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -3,11 +3,9 @@ require 'redirect_creator'
 
 Awestruct::Extensions::Pipeline.new do
   helper Awestruct::Extensions::GoogleAnalytics
-  Awestruct::Extensions::Pipeline.new do
-    extension Awestruct::Extensions::Sitemap.new
-  end
   # extension Awestruct::Extensions::Posts.new '/news'
   extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Sitemap.new
   # Indexifier *must* come before Atomizer
   # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
   extension Awestruct::Extensions::RedirectCreator.new
